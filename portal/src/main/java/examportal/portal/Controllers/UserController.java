2800d313d3a9ffbab24e91ff2c0fd1149fca89c2
devendrapanwar22 <121929086+devendrapanwar22@users.noreply.github.com>
2023-10-28 15:57:46 +0530

    update code user and orgnization apis

---
+++ portal/src/main/java/examportal/portal/Controllers/UserController.java
@@ -0,0 +1,26 @@
+package examportal.portal.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import examportal.portal.Entity.User;
+import examportal.portal.Services.UserService;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/user")
+    public ResponseEntity<User> createuser(@RequestBody User user)
+    {
+        User savedUser = this.userService.createUser(user);
+        return new ResponseEntity<User>(savedUser,HttpStatus.CREATED);
+    }
+    
+}
---
+++ portal/src/main/java/examportal/portal/Repo/UserRepo.java
@@ -0,0 +1,11 @@
+package examportal.portal.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import examportal.portal.Entity.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,String>{
+    
+}
