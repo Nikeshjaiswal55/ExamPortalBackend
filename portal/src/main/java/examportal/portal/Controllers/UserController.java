728bc93a70e80cb9c79236a9053138992b73465d
devendrapanwar22 <121929086+devendrapanwar22@users.noreply.github.com>
2023-11-02 15:20:07 +0530

    cross origin added

--- portal/src/main/java/examportal/portal/Controllers/UserController.java
+++ portal/src/main/java/examportal/portal/Controllers/UserController.java
@@ -3,6 +3,7 @@ package examportal.portal.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import examportal.portal.Entity.User;
 import examportal.portal.Services.UserService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
