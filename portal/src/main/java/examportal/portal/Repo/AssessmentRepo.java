48b47fcfe4dd19b78216f8fe4462ec6db64ff812
Krishna Sadhu <Krishnas.bca2022@ssism.org>
2023-11-20 17:19:59 +0530

    do some change in assesmentController

--- portal/src/main/java/examportal/portal/Repo/AssessmentRepo.java
+++ portal/src/main/java/examportal/portal/Repo/AssessmentRepo.java
@@ -13,5 +13,9 @@ public interface AssessmentRepo extends JpaRepository<Assessment,String> {
 
     @Query("SELECT a FROM Assessment a WHERE a.orgnizationId=:orgnizationId")
     List<Assessment> getAssessmentsBy_orgnizationId(@Param("orgnizationId") String orgnizationId);
+
+
+    @Query("SELECT a FROM Assessment a WHERE a.userId=:userId")
+    List<Assessment> getAssessmentsBy_userId(@Param("userId") String userId);
 }
 
