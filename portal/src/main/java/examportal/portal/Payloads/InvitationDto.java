8ded3ac8ad9a664e124eb83c88d0d8b22a149015
devendra-singh-rajput <devendras.bca2022@ssism.org>
2023-12-05 15:51:21 +0530

    h
    tyhjh

---
+++ portal/src/main/java/examportal/portal/Payloads/InvitationDto.java
@@ -0,0 +1,12 @@
+package examportal.portal.Payloads;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class InvitationDto {
+    private String paperID;
+    private String orgnizationId;
+    private List<String> emails;
+    private String token;
+}
