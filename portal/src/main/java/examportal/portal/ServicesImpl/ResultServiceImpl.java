e16dc5e0be5aee69d0d3c4d981539f103ec70ae1
devendrapanwar22 <121929086+devendrapanwar22@users.noreply.github.com>
2023-12-09 15:22:08 +0530

    gghggh

--- portal/src/main/java/examportal/portal/ServicesImpl/ResultServiceImpl.java
+++ portal/src/main/java/examportal/portal/ServicesImpl/ResultServiceImpl.java
@@ -301,7 +301,7 @@ public class ResultServiceImpl implements ResultService {
         }
 
         ResultDto dto = new ResultDto();
-        dto.setResultID(result.getResultID());
+        // dto.setResultID(result.getResultID());
         // dto.setCheating(cheating);
         dto.setResult(result);
         dto.setQuestions(questions);
