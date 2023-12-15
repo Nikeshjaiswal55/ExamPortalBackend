23dd4bcc8cb10590bbd5a1c871322e207d27dada
Krishna Sadhu <Krishnas.bca2022@ssism.org>
2023-12-15 11:14:13 +0530

    delete all

--- portal/src/main/java/examportal/portal/Services/ExamDetailsService.java
+++
@@ -1,8 +0,0 @@
-package examportal.portal.Services;
-
-import examportal.portal.Entity.ExamDetails;
-
-public interface ExamDetailsService {
-    ExamDetails createExamDetails(ExamDetails examDetails);
-    ExamDetails updateExamDetails(ExamDetails examDetails);
-}
